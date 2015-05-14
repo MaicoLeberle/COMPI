6361c894b0740cecc4382bc1372981e152868038
Mallku2 <mallkuernesto@gmail.com>
2015-05-14 17:23:51 -0300

    Fixed many many errors from parser.y and node2.h; now compiles...

--- src/node2.h
+++ src/node2.h
@@ -13,49 +13,52 @@ public:
 class node_class_decl;
 class node_program : public node {
 public:
-    std::vector<node_class_decl*> classes;
+    std::vector<node_class_decl*> *classes;
     // Constructors are defined to receive just the information
     // available in the semantic actions where a new
     // node must be constructed.
-    program(node_class_decl *a_class){ 
-        ids = new std::vector<node_class_decl*>(a_class);
+    node_program(node_class_decl *a_class){ 
+        classes = new std::vector<node_class_decl*>();
+        classes->push_back(a_class);
     }
     //virtual llvm::Value* codeGen(CodeGenContext& context) { }
-};
-
-class node_class_decl : public node {
-public:
-    std::string id;
-    std:vector<node_class_block*> *class_block;
-    class_decl(std::string ident, std:vector<node_class_block*> *c_block) :
-        id(ident), class_block(c_block) { }
-    //virtual llvm::Value* codeGen(CodeGenContext& context);
 };
 
 class node_class_block : public node {
     
 };
 
-
-class node_field_decl : public node_class_block {
+class node_class_decl : public node {
 public:
-    int type;
-    node_ids *ids;
-    field_decl(int t, node_ids *list_id) : type(t), ids(list_id) { }
+    std::string id;
+    std::vector<node_class_block*> *class_block;
+    node_class_decl(std::string ident, std::vector<node_class_block*> *c_block) :
+        id(ident), class_block(c_block) { }
     //virtual llvm::Value* codeGen(CodeGenContext& context);
 };
 
+
 class node_ids : public node {
 public:
-    std::vector<std::string*> ids;
+    std::vector<std::string*> *ids;
     int index;
     node_ids(std::string* id){ 
-        ids = new std::vector<std::string*>(id);
+        ids = new std::vector<std::string*>();
+        ids->push_back(id);
     }
     node_ids(std::string* id, int ind){ 
-        ids = new std::vector<std::string*>(id);
+        ids = new std::vector<std::string*>();
+        ids->push_back(id);
         index = ind;
     }
+    //virtual llvm::Value* codeGen(CodeGenContext& context);
+};
+
+class node_field_decl : public node_class_block {
+public:
+    std::string *type;
+    node_ids *ids;
+    node_field_decl(std::string * t, node_ids *list_id) : type(t), ids(list_id) { }
     //virtual llvm::Value* codeGen(CodeGenContext& context);
 };
 
@@ -63,51 +66,70 @@ class node_body;
 class node_typed_identifier;
 class node_method_decl : public node_class_block {
 public:
-    std::string m_type;
-    std::string id;
+    std::string *m_type;
+    std::string *id;
     std::vector<node_typed_identifier*> *formal_params;
     node_body *m_body;
-    method_decl(std::string method_type, std::string method_id, 
+    node_method_decl(std::string *method_type, std::string *method_id, 
     std::vector<node_typed_identifier*> *method_formal_params, node_body *method_body) :
     m_type(method_type), id(method_id), formal_params(method_formal_params), 
-    m_body(method_body);
+    m_body(method_body) {}
     //virtual llvm::Value* codeGen(CodeGenContext& context);
 };
 
 class node_typed_identifier :public node {
 public:
-    int type;
-    std::string id;
-    typed_identifier(int i_type, std::string i_value) : type(i_type), id(i_value) { }
-};
-
-class node_statement;
-class node_body : public node {
-public:
-    // TODO: invariante de clase: extern_body <=> b_block == NULL ?
-    bool extern_body;
-    std::vector<node_statement*> *b_block;
-    node_body(std::vector<node_statements*> *body_block) : extern_body(false), b_block(body_block) { }
-    node_body() : extern_body(true), b_block(NULL) { }
-    bool is_extern() { return extern_body; }
-    //virtual llvm::Value* codeGen(CodeGenContext& context);
+    std::string *type;
+    std::string *id;
+    node_typed_identifier(std::string *i_type, std::string *i_value) : type(i_type), id(i_value) { }
 };
 
 class node_statement : public node {
 
 };
 
-class node_location : public node_expr {
+// A block of statements, that defines a new local scope
+class node_block : public node {
 public:
-    std::vector<string*> *ids;
-    node_expr* e;
-    location(std::vector<string*> location_ids) : ids(location_ids) { }
-    location(std::vector<string*> location_ids, node_expr* index) :
-        ids(location_ids), e(index) { }
+    std::vector<node_statement*> *block_content;
+    node_block(std::vector<node_statement*> *content) : block_content(content) { }
     //virtual llvm::Value* codeGen(CodeGenContext& context);
 };
 
+
+class node_body : public node {
+public:
+    // TODO: invariante de clase: extern_body <=> b_block == NULL ?
+    bool extern_body;
+    node_block *b_block;
+    node_body(node_block *body_block) : extern_body(false), b_block(body_block) { }
+    node_body() : extern_body(true), b_block(NULL) { }
+    bool is_extern() { return extern_body; }
+    //virtual llvm::Value* codeGen(CodeGenContext& context);
+};
+
+
+
+
+class node_field_decl_statement : public node_statement{ 
+public:
+    node_field_decl *field_decl;
+    node_field_decl_statement(node_field_decl *f_d) : field_decl(f_d) { }
+    //virtual llvm::Value* codeGen(CodeGenContext& context);
+};
+
+// To let a node_block, be used as a node_statement
+class node_block_statement : public node_statement {
+public:
+    node_block* s;
+    node_block_statement(node_block* statement_block) : s(statement_block) { }
+    //virtual llvm::Value* codeGen(CodeGenContext& context);
+};
+
+
+class node_location;
 class node_expr;
+
 class node_assignment_statement : public node_statement {
 public:
     node_location* loc;
@@ -119,14 +141,14 @@ public:
     //virtual llvm::Value* codeGen(CodeGenContext& context);
 };
 
-class node_method_call : public node {
+// To make possible to consider a method call as a statement, we wrap
+// the node_method_call object in the following class, that inherites from
+// node_statement
+class node_method_call;
+class node_method_call_statement : public node_statement {
 public:
-    std::vector<std::string*> *ids;
-    std::vector<node_expr*> *actual_parameters;
-    node_method_call(std::vector<std::string*> *list_ids) : ids(list_ids) { }
-    node_method_call(std::vector<std::string*> *list_ids, std::vector<node_expr*> *expressions) :
-        ids(list_ids), actual_parameters(expressions) { }
-    //virtual llvm::Value* codeGen(CodeGenContext& context);
+    node_method_call* m;
+    node_method_call_statement(node_method_call *method) : m(method) { }
 };
 
 class node_if_statement : public node_statement {
@@ -165,6 +187,7 @@ public:
 class node_return_statement : public node_statement {
 public:
     node_expr* e;
+    node_return_statement() { e = NULL; }
     node_return_statement(node_expr* return_expression) : e(return_expression) { }
     //virtual llvm::Value* codeGen(CodeGenContext& context);
 };
@@ -215,51 +238,61 @@ public:
 
 class node_string_literal : public node_literal { 
 public:
-    std::string value;
-    node_string_literal(const std::string val) : value(val) { }
+    std::string *value;
+    node_string_literal(std::string *val) : value(val) { }
     //virtual llvm::Value* codeGen(CodeGenContext& context);
 };
 
-class node_binary_operation_expr : public expr {
+class node_binary_operation_expr : public node_expr {
 public:
     char op;
-    expr* lhs_expr;
-    expr* rhs_expr;
-    node_binary_operation_expr(char binary_operator, expr* left_expression, expr* right_expression) :
+    node_expr* lhs_expr;
+    node_expr* rhs_expr;
+    node_binary_operation_expr(char binary_operator, node_expr* left_expression, 
+    node_expr* right_expression) :
         op(binary_operator), lhs_expr(left_expression), rhs_expr(right_expression) { }
     //virtual llvm::Value* codeGen(CodeGenContext& context);
 };
 
 class node_location : public node_expr {
 public:
-    std::vector<std::string*> ids;
+    std::vector<std::string*> *ids;
     node_expr* e;
-    location(std::vector<std::string*> location_ids) : ids(location_ids) { }
-    location(std::vector<std::string*> location_ids, node_expr* index) :
+    node_location(std::vector<std::string*> *location_ids) : ids(location_ids) { }
+    node_location(std::vector<std::string*> *location_ids, node_expr* index) :
         ids(location_ids), e(index) { }
     //virtual llvm::Value* codeGen(CodeGenContext& context);
 };
 
 class node_negate_expr : public node_expr {
 public:
-    node_expr e;
-    node_negate_expr(node_expr expression) : e(expression) { }
+    node_expr *e;
+    node_negate_expr(node_expr *expression) : e(expression) { }
     //virtual llvm::Value* codeGen(CodeGenContext& context);
 };
 
 class node_minus_expr : public node_expr {
 public:
-    node_expr e;
-    node_minus_expr(node_expr expression) : e(expression) { }
+    node_expr *e;
+    node_minus_expr(node_expr *expression) : e(expression) { }
     //virtual llvm::Value* codeGen(CodeGenContext& context);
 };
 
 class node_parenthesis_expr : public node_expr {
 public:
-    node_expr e;
-    node_parenthesis_expr(node_expr expression) : e(expression) { }
+    node_expr *e;
+    node_parenthesis_expr(node_expr *expression) : e(expression) { }
     //virtual llvm::Value* codeGen(CodeGenContext& context);
 };
 
+class node_method_call : public node_expr {
+public:
+    std::vector<std::string*> *ids;
+    std::vector<node_expr*> *actual_parameters;
+    node_method_call(std::vector<std::string*> *list_ids) : ids(list_ids) { }
+    node_method_call(std::vector<std::string*> *list_ids, std::vector<node_expr*> *expressions) :
+        ids(list_ids), actual_parameters(expressions) { }
+    //virtual llvm::Value* codeGen(CodeGenContext& context);
+};
 
 #endif
--- src/parser/parser.y
+++ src/parser/parser.y
@@ -22,13 +22,13 @@ node_program* ast; // Pointer to the AST
     std::vector<node_typed_identifier*> *params;
     node_ids *ids;
     node_body *body;
-    std::vector<node_statement*> *block;
+    node_block *block;
     node_statement *statement;
+    std::vector<node_statement*> *statements;
     std::vector<std::string*> *ids_reference;
     std::vector<node_expr*> *expr_params;
     node_literal *literal;
     node_expr *expr;
-    std::vector<node_identifier*> ids_reference;
     bool l_bool;
 	int l_int;
 	float l_float;
@@ -62,7 +62,8 @@ node_program* ast; // Pointer to the AST
 %type <token> assign_op
 %type <params> params
 %type <body> body
-%type <block> block statements
+%type <block> block
+%type <statements> statements
 %type <statement> statement
 %type <ids_reference> ids_reference
 %type <expr_params> expr_params
@@ -71,9 +72,9 @@ node_program* ast; // Pointer to the AST
 
 %%
 program
-	: program class_decl    {$1->classes.push_back($2);
+	: program class_decl    {$1->classes->push_back($2);
                              $$ = $1;}
-	| class_decl            {$$ = new node_program(new std::vector<node_class_decl*>($1));
+	| class_decl            {$$ = new node_program($1);
                              ast = $$;}
 	;
 
@@ -83,8 +84,8 @@ class_decl
 
 class_block
 	:                              {$$ = new std::vector<node_class_block*>();}
-	| class_block field_decl       {$1.push_back($2); $$ = $1;}
-	| class_block method_decl      {$1.push_back($2); $$ = $1;}
+	| class_block field_decl       {$1->push_back($2); $$ = $1;}
+	| class_block method_decl      {$1->push_back($2); $$ = $1;}
 	;
 
 field_decl
@@ -92,15 +93,15 @@ field_decl
 	;
 
 ids
-	: ids ',' ID                   {$1->ids.push_back($3); $$ = $1;}
+	: ids ',' ID                   {$1->ids->push_back($3); $$ = $1;}
 	| ID                           {$$ = new node_ids($1);}
-	| ids ',' ID '[' L_INT ']'     {$1->ids.push_back($3); $1->index = $3; $$ = $1;}
+	| ids ',' ID '[' L_INT ']'     {$1->ids->push_back($3); $1->index = $5; $$ = $1;}
 	| ID '[' L_INT ']'             {$$ = new node_ids($1, $3);}
 	;
 
 method_decl
-	: method_type ID '(' params ')' body    {$$ = new node_method_decl($1, *$2, $6);}
-    | method_type ID '(' ')' body           {$$ = new node_method_decl($1, NULL, $5);}
+	: method_type ID '(' params ')' body    {$$ = new node_method_decl($1, $2, $4, $6);}
+    | method_type ID '(' ')' body           {$$ = new node_method_decl($1, $2, NULL, $5);}
 	;
 
 method_type
@@ -112,14 +113,15 @@ type
 	: INT                                   {$$ = new std::string("int");}
     | FLOAT                                 {$$ = new std::string("float");}
     | BOOLEAN                               {$$ = new std::string("boolean");}
-    | ID                                    {$$ = $<type>1}
+    | ID                                    {$$ = $<type>1;}
 	;
 
 params
 	: params ',' type ID            {$1->push_back(new node_typed_identifier($3, $4));
                                      $$ = $1;}
 	| type ID                       {$$ = new std::vector<node_typed_identifier*> 
-                                          (new node_typed_identifier($1, $2));}
+                                          ();
+                                    $$->push_back(new node_typed_identifier($1, $2));}
 	;
 
 body
@@ -128,32 +130,31 @@ body
 	;
 
 block
-	: '{' statements '}'            {$$ = $2;}
+	: '{' statements '}'            {$$ = new node_block($2);}
 	;
 
 statements
 	:                               {$$ = new std::vector<node_statement*>();}
 	| statements statement          {$1->push_back($2); $$ = $1;}
-	| statement                     {$$ = new std::vector<node_statement*>($1);}
+	| statement                     {$$ = new std::vector<node_statement*>();
+                                    $$->push_back($1);}
 	;
 
 statement
-	: field_decl                                 {$$ = new node_statement();//TODO: y aca?
-                                                 }
-	| location assign_op expr ';'                {$$ = new node_assignment_statement($1, $2, $3);}
-	| method_call ';'                            {$$ = $1; //TODO: y aca?
-                                                    }
+	: field_decl                                 {$$ = new node_field_decl_statement($1);}
+	| location assign_op expr ';'                {$$ = new node_assignment_statement((node_location*)$1, 
+                                                       $2, $3);}
+	| method_call ';'                            {$$ = new node_method_call_statement((node_method_call*)$1);}
 	| IF '(' expr ')' statement                  {$$ = new node_if_statement($3, $5);}
     | IF '(' expr ')' statement ELSE statement   {$$ = new node_if_statement($3, $5, $7);}
 	| FOR ID '=' expr ',' expr statement         {$$ = new node_for_statement($2, $4, $6, $7);}
 	| WHILE expr statement                       {$$ = new node_while_statement($2, $3);}
 	| RETURN expr ';'                            {$$ = new node_return_statement($2);}
-    | RETURN ';'                            {$$ = new node_return_statement();}
+    | RETURN ';'                                 {$$ = new node_return_statement();}
 	| BREAK ';'                                  {$$ = new node_break_statement();}
 	| CONTINUE ';'                               {$$ = new node_continue_statement();}
 	| ';'                                        {$$ = new node_skip_statement();}
-	| block                                      {$$ = $1; //TODO: y aca?
-                                                 }
+	| block                                      {$$ = new node_block_statement($1);}
 	;
 
 assign_op
@@ -179,14 +180,15 @@ location
 ids_reference
 	:                                       {$$ = new std::vector<std::string*>();}
 	| ids_reference '.' ID                  {$1->push_back($3); $$ = $1;}
-	| '.' ID                                {$$ = new std::vector<std::string*>($2);}
+	| '.' ID                                {$$ = new std::vector<std::string*>();
+                                             $$->push_back($2);}
 	;
 
 expr
 	: location
 	| method_call
 	| literal
-	| expr bin_op expr                      {$$ = new node_binary_operation_expr($1, $2, $3);}
+	| expr bin_op expr                      {$$ = new node_binary_operation_expr($2, $1, $3);}
 	| '-' expr                              {$$ = new node_minus_expr($2);}
 	| '!' expr                              {$$ = new node_negate_expr($2);}
 	| '(' expr ')'                          {$$ = new node_parenthesis_expr($2);}
@@ -194,7 +196,8 @@ expr
 
 expr_params
 	: expr_params ',' expr                  {$1->push_back($3); $$ = $1;}
-	| expr                                  {$$ = new std::vector<node_expr*> ($1);}
+	| expr                                  {$$ = new std::vector<node_expr*> ();
+                                             $$->push_back($1);}
 	;
 
 bin_op
@@ -230,10 +233,10 @@ cond_op
 	;
 
 literal
-	: L_INT                     {$$ = $<token>1;}
-	| L_FLOAT                   {$$ = $<token>1;}
-	| L_BOOL                    {$$ = $<token>1;}
-	| L_STR                     {$$ = $<token>1;}
+	: L_INT                     {$$ = new node_int_literal($1);}
+	| L_FLOAT                   {$$ = new node_float_literal($1);}
+	| L_BOOL                    {$$ = new node_bool_literal($1);}
+	| L_STR                     {$$ = new node_string_literal($1);}
 	;
 
 %%
