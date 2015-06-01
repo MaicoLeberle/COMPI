f6338a4f92b934aa703b704d5a1557b7351c9b22
Maico Leberle <maico.leberle@gmail.com>
2015-06-01 17:13:48 -0300

    Some changes and comments in the symtable module.

--- src/symtable.h
+++ src/symtable.h
@@ -3,7 +3,8 @@
 
 #include <cassert>
 
-
+/*  This class represents the information stored for 
+    each identifier.                                    */
 class symtable_element{
 private:
     /*  Identifier class: function, variable or array.  */
@@ -20,7 +21,8 @@ private:
     std::list<symtable_element> func_params = NULL;
 
 public:
-
+    /*  The identifier class is deducted from the 
+        constructor parameters.                         */
     symtable_element(id_type t) : c_id(T_VAR), t_id(t) { }
     symtable_element(id_type t, std::list<symtable_element> f) : 
         c_id(T_FUNCTION), t_id(t), func_params(f) { }
@@ -28,6 +30,8 @@ public:
         c_id(T_ARRAY), t_id(t), dim(d) { }
 };
 
+/*  This class represents the symbol table associated 
+    with the current class/block/function.              */
 class symtable { 
 private:
     std::unordered_map<std::string, symtable_element> hashtable;
@@ -47,6 +51,8 @@ public:
     }
 } ;
 
+/*  This class represents the chain of symbols table
+    that conform the current scope.                      */
 class scopes_list {
 private:
     symtable table;
@@ -56,15 +62,25 @@ public:
     scopes_list() : prev(NULL) { }
     
     symtable_element* get (std::string key) {
+        /*  Must search from the top of the stack and downwards.        */
         for(scopes_list *it = this; this != NULL; this = this->prev) {
             if((it->table).elem_exists(key))
                 return ((it->table).get(key));
         }
+        /*  If the key has not been found in any of the symbols 
+            tables, then it has not been found in the current scope.     */
         return NULL;
     }
 
     void put(std::string key, symtable_element value) {
+        /*  Always insert a new identifier's information in the top
+            of the stack.                                               */
         table.put(key, value);
+    }
+
+    ~scopes_list() {
+        if (prev != NULL)
+            delete prev;
     }
 };
 
