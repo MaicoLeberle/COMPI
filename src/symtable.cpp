f141a3093b4cd1b09377375cca01442f7dba80f8
Maico Leberle <maico.leberle@gmail.com>
2015-06-02 21:32:28 -0300

    Added some comments and arranged the code more orderly.

--- src/symtable.cpp
+++ src/symtable.cpp
@@ -1,98 +1,15 @@
-#include <cassert>
-#include <string>
-#include <list>
-#include <unordered_map>
+#include "symtable.h"
 
-/*  This class represents the information stored for each identifier.        */
-class symtable_element {
-
-public:
-    /*  Identifier class: function, variable or array.                       */
-    enum id_class { T_FUNCTION
-                  , T_CLASS
-                  , T_VAR
-                  , T_OBJ
-                  , T_ARRAY
-                  , T_OBJ_ARRAY };
-    id_class c_id;
-    /*  Identifier type. It always has one.                                  */
-    enum id_type { INTEGER
-                 , BOOLEAN
-                 , FLOAT
-                 , VOID
-                 , ID };
-    /*  Basic type variable.                            */
-    symtable_element(id_type t);
-    /*  Basic type array.                               */
-    symtable_element(id_type t, unsigned int d);
-    /*  Object.                                         */
-    symtable_element(std::string* s);  
-    /*  Objects array.                                  */
-    symtable_element(std::string* s, unsigned int d);
-    /*  Function.                                       */
-    symtable_element(id_type t, std::list<std::string>* f); 
-    /*  Class.                                          */
-    symtable_element(std::string* s, std::list<std::string>* f);
-
-    /*  Getters.                                        */
-    id_class get_class ();
-    id_type get_type ();
-    std::string* get_class_type ();
-    unsigned int get_dimension ();
-    std::list<std::string>* get_func_params ();
-    std::list<std::string>* get_class_fields ();
-private:
-    id_type t_id;
-    /*  Identifier type when it is an object.                                */
-    std::string* class_type;
-    /*  Identifier dimension. Only for arrays.                               */
-    unsigned int dim = 0;
-    /*  List of formal arguments (keys to the hashtable) if the identifier 
-        is a function. Note that every element in the list should be of a 
-        basic type.                                                          */
-    std::list<std::string>* func_params = NULL;
-    /*  List of attributes and methods (keys to the hastable, actually) if 
-        the identifier is a class. Every element in the list should be 
-        avalid entry in some symbols table. Therefore, recursion can be 
-        done. This information will be useful when checking method calling.  */
-    std::list<std::string>* class_fields = NULL;
-};
-
-/*  This class represents the symbol table associated 
-    with the current class/block/function.              */
-class symtable { 
-private:
-    std::unordered_map<std::string, symtable_element> hashtable;
-
-public:
-    bool elem_exists (std::string key);
-
-    symtable_element get(std::string key);
-
-    void put (std::string key, symtable_element value);
-} ;
-
-/*  This class represents the chain of symbols table
-    that conform the current scope.                      */
-class scopes_list {
-private:
-    symtable table;
-    scopes_list *prev;
-
-public:
-    scopes_list() : prev(NULL) { }
-    
-    symtable_element* get (std::string key);
-
-    void put(std::string key, symtable_element value);
-
-    ~scopes_list();
-};
 
 /*  symtable_entry methods.                                                  */
 
 /*  Each constructor receives the minimum amount of information necessary to
     resolve the id_class, id_type, etc., that the identifier has.            */
+
+symtable_element::symtable_element(id_class not_found_class) 
+    : c_id(not_found_class) {
+        assert(not_found_class == NOT_FOUND);
+}
 
 symtable_element::symtable_element(id_type t) :
     c_id(T_VAR), t_id(t) { }
@@ -121,12 +38,12 @@ symtable_element::id_type symtable_element::get_type () {
 }
 
 std::string* symtable_element::get_class_type () {
-    assert(c_id == T_VAR && t_id == ID);
+    assert(c_id == T_OBJ || c_id == T_OBJ_ARRAY || c_id == T_CLASS);
     return (class_type); 
 }
 
 unsigned int symtable_element::get_dimension () { 
-    assert(c_id == T_ARRAY); 
+    assert(c_id == T_ARRAY || c_id == T_OBJ_ARRAY); 
     return dim; 
 }
 
@@ -148,47 +65,24 @@ bool symtable::elem_exists (std::string key) {
 
 symtable_element symtable::get(std::string key) {
     assert(elem_exists(key));
-    return (hashtable[key]);
+    return ((hashtable.find(key))->second);
 }
 
 void symtable::put (std::string key, symtable_element value) {
-    hashtable[key] = value;
+    hashtable.insert(std::pair<std::string, symtable_element>(key, value));
 }
 
 /*  scopes_list methods.                                                     */
 
-symtable_element* scopes_list::get(std::string key) {
+symtable_element scopes_list::get(std::string key) {
      /*  Must search from the top of the stack and downwards.        */
-    for(scopes_list *it = this; it != NULL; it = it->prev) {
-        if((it->table).elem_exists(key)) {
-            symtable_element e = (it->table).get(key);
-            symtable_element* ret_elem;
-            switch (e.get_class()) {
-                case (symtable_element::T_FUNCTION):
-                    ret_elem = new symtable_element(e.get_type(), e.get_func_params());
-                    break;
-                case (symtable_element::T_CLASS):
-                    ret_elem = new symtable_element(e.get_class_type(), e.get_class_fields());
-                    break;
-                case (symtable_element::T_VAR):
-                    ret_elem = new symtable_element(e.get_type());
-                    break;
-                case (symtable_element::T_OBJ):
-                    ret_elem = new symtable_element(e.get_class_type());
-                    break;
-                case (symtable_element::T_ARRAY):
-                    ret_elem = new symtable_element(e.get_type(), e.get_dimension());
-                    break;
-                case (symtable_element::T_OBJ_ARRAY):
-                    ret_elem = new symtable_element(e.get_class_type(), e.get_dimension());
-                    break;
-            }
-            return (ret_elem);
-        }
-    }
+    for(scopes_list *it = this; it != NULL; it = it->prev) 
+        if((it->table).elem_exists(key))
+            return ((it->table).get(key));
+
     /*  If the key has not been found in any of the symbols 
         tables, then it has not been found in the current scope.     */
-    return (NULL);
+    return (symtable_element(symtable_element::NOT_FOUND));
 }
 
 void scopes_list::put(std::string key, symtable_element value) {
@@ -202,4 +96,4 @@ scopes_list::~scopes_list() {
         delete prev;
 }
 
-int main() { return 0; }
+int main () { std::cout << sizeof(symtable_element) << std::endl;}
--- src/symtable.h
+++ src/symtable.h
@@ -1,60 +1,98 @@
-#ifdef _SYMTABLE_H_
+#ifndef _SYMTABLE_H_
 #define _SYMTABLE_H_
 
 #include <cassert>
+#include <map>
+#include <string>
+#include <list>
 
 
 /*  This class represents the information stored for each identifier.        */
 class symtable_element {
 public:
-    /*  Identifier class: function, variable or array.                       */
+    /*  Identifier class. It will tell what kind of identifier it is.        */
     enum id_class { T_FUNCTION
                   , T_CLASS
                   , T_VAR
                   , T_OBJ
                   , T_ARRAY
-                  , T_OBJ_ARRAY };
-    id_class c_id;
-    /*  Identifier type. It always has one.                                  */
+                  , T_OBJ_ARRAY
+                  , NOT_FOUND };
+    /*  Identifier type.   It will represent the type of the identifier.
+        Objects and array of objects will have the type of the class they 
+        belong to.                                                           */
     enum id_type { INTEGER
                  , BOOLEAN
                  , FLOAT
                  , VOID
                  , ID };
-    /*  Basic type variable.                            */
-    symtable_element(id_type t);
-    /*  Basic type array.                               */
-    symtable_element(id_type t, unsigned int d);
-    /*  Object.                                         */
-    symtable_element(std::string* s);  
-    /*  Objects array.                                  */
-    symtable_element(std::string* s, unsigned int d);
-    /*  Function.                                       */
-    symtable_element(id_type t, std::list<std::string>* f); 
-    /*  Class.                                          */
-    symtable_element(std::string* s, std::list<std::string>* f);
 
-    /*  Getters.                                        */
+    /*  For the following constructors, only necessary information is given. 
+        Other attributes will be inferred from this information.             */
+
+    /*  To be returned when an entry is not found in a symbols table.
+        Precondition: the argument is NOT_FOUND                              */
+    symtable_element(id_class);
+
+    /*  Basic type variable.                                                 */
+    symtable_element(id_type);
+
+    /*  Basic type array.                                                    */
+    symtable_element(id_type, unsigned int);
+
+    /*  Object.                                                              */
+    symtable_element(std::string*); 
+
+    /*  Objects array.                                                       */
+    symtable_element(std::string*, unsigned int);
+
+    /*  Function.                                                            */
+    symtable_element(id_type, std::list<std::string>*); 
+
+    /*  Class.                                                               */
+    symtable_element(std::string*, std::list<std::string>*);
+
+
+    /*  Getters.                                                             */
+
+    /*  Returns the corresponding enum id_class value.                       */
     id_class get_class ();
+
+    /*  Returns the corresponding enum id_type value.                        */
     id_type get_type ();
+
+    /*  Precondition: get_class() == (T_OBJ || T_OBJ_ARRAY || T_CLASS).      */
     std::string* get_class_type ();
+
+    /*  Precondition: get_class() = (T_ARRAY || T_OBJ_ARRAY).                */
     unsigned int get_dimension ();
+
+    /*  Returns list of function formal parameters identifiers.
+        Precondition: get_class() == T_FUNCTION.                             */
     std::list<std::string>* get_func_params ();
+
+    /*  Returns list of class fields and methods identifiers.
+        Precondition: get_class() == T_CLASS.                                */
     std::list<std::string>* get_class_fields ();
+
 private:
+    id_class c_id;
+
     id_type t_id;
+
     /*  Identifier type when it is an object.                                */
     std::string* class_type;
+
     /*  Identifier dimension. Only for arrays.                               */
     unsigned int dim = 0;
-    /*  List of formal arguments (keys to the hashtable) if the identifier 
-        is a function. Note that every element in the list should be of a 
-        basic type.                                                          */
+
+    /*  List of formal arguments identifiers (meant for when the symbols 
+        table element is a function). Note that every element in the list 
+        should be of a basic type.                                           */
     std::list<std::string>* func_params = NULL;
-    /*  List of attributes and methods (keys to the hastable, actually) if 
-        the identifier is a class. Every element in the list should be 
-        avalid entry in some symbols table. Therefore, recursion can be 
-        done. This information will be useful when checking method calling.  */
+
+    /*  List of attributes and methods identifiers (meant for when the 
+        symbols table element is a class).                                   */
     std::list<std::string>* class_fields = NULL;
 };
 
@@ -62,33 +100,40 @@ private:
 /*  This class represents the symbol table associated 
     with the current class/block/function.              */
 class symtable { 
-private:
-    std::unordered_map<std::string, symtable_element> hashtable;
-
 public:
-    bool elem_exists (std::string key);
+    bool elem_exists (std::string);
 
-    symtable_element get(std::string key);
+    /*  Precondition: the formal argument given to get should already exist
+        in the symtable; i.e., elem_exists(...) == TRUE.                     */
+    symtable_element get(std::string);
 
-    void put (std::string key, symtable_element value);
+    void put (std::string, symtable_element);
+
+private:
+    std::map<std::string, symtable_element> hashtable;
 } ;
 
 
-/*  This class represents the chain of symbols table
-    that conform the current scope.                      */
+/*  This class represents the stack of symbol tables that conform the current
+    scope.                                                                   */
 class scopes_list {
 private:
     symtable table;
     scopes_list *prev;
 
 public:
-    scopes_list() : prev(NULL) { }
-    
-    symtable_element* get (std::string key);
+    /*  This constructor is meant only for the empty stack.                  */
+    scopes_list(void);
 
-    void put(std::string key, symtable_element value);
+    /*  The scopes_list* parameter should represent the previous top of the
+        symbol tables stack.                                                 */
+    scopes_list(scopes_list*);
+    
+    symtable_element get (std::string);
+
+    void put(std::string, symtable_element);
 
     ~scopes_list();
 };
 
-#endif _SYMTABLE_H_
+#endif
