c9b25111d77bc6db49e3dc62a03466d43e680ab9
Maico Leberle <maico.leberle@gmail.com>
2015-06-04 11:13:55 -0300

    Modified the symtable module to better reflect the future scope resolution needs.

--- src/symtable.cpp
+++ src/symtable.cpp
@@ -6,28 +6,32 @@
 /*  Each constructor receives the minimum amount of information necessary to
     resolve the id_class, id_type, etc., that the identifier has.            */
 
-symtable_element::symtable_element(id_class not_found_class) 
-    : c_id(not_found_class) {
-        assert(not_found_class == NOT_FOUND);
+symtable_element::symtable_element(id_class not_found_class) :
+    c_id(not_found_class) {
+    assert(not_found_class == NOT_FOUND);
 }
 
-symtable_element::symtable_element(id_type t) :
-    c_id(T_VAR), t_id(t) { }
+symtable_element::symtable_element(std::string k, id_type t) :
+    key(k), c_id(T_VAR), t_id(t) { }
 
-symtable_element::symtable_element(id_type t, unsigned int d) : 
-    c_id(T_ARRAY), t_id(t), dim(d) { }
+symtable_element::symtable_element(std::string k, id_type t, unsigned int d) : 
+    key(k), c_id(T_ARRAY), t_id(t), dim(d) { }
 
-symtable_element::symtable_element(std::string* s) : 
-    c_id(T_OBJ), t_id(ID), class_type(s) { }  
+symtable_element::symtable_element(std::string k, std::string s) : 
+    key(k), c_id(T_OBJ), t_id(ID), class_type(s) { }  
 
-symtable_element::symtable_element(std::string* s, unsigned int d) :
-    c_id(T_OBJ_ARRAY), t_id(ID), class_type(s) { }
+symtable_element::symtable_element(std::string k, std::string s, unsigned int d) :
+    key(k), c_id(T_OBJ_ARRAY), t_id(ID), class_type(s) { }
 
-symtable_element::symtable_element(id_type t, std::list<std::string>* f) :
-     c_id(T_FUNCTION), t_id(t), func_params(f) { } 
+symtable_element::symtable_element(std::string k, id_type t, std::list<symtable_element>* f) :
+    key(k), c_id(T_FUNCTION), t_id(t), func_params(f) { } 
 
-symtable_element::symtable_element(std::string* s, std::list<std::string>* f) :
-    c_id(T_CLASS), class_fields(f), class_type (s) { }
+symtable_element::symtable_element(std::string k, std::list<symtable_element>* f) :
+    key(k), c_id(T_CLASS), class_fields(f), class_type (k) { }
+
+std::string symtable_element::get_key() {
+    return (this->key);
+}
 
 symtable_element::id_class symtable_element::get_class () { 
     return c_id; 
@@ -37,7 +41,7 @@ symtable_element::id_type symtable_element::get_type () {
     return t_id; 
 }
 
-std::string* symtable_element::get_class_type () {
+std::string symtable_element::get_class_type () {
     assert(c_id == T_OBJ || c_id == T_OBJ_ARRAY || c_id == T_CLASS);
     return (class_type); 
 }
@@ -47,12 +51,12 @@ unsigned int symtable_element::get_dimension () {
     return dim; 
 }
 
-std::list<std::string>* symtable_element::get_func_params () {
+std::list<symtable_element>* symtable_element::get_func_params () {
     assert(c_id == T_FUNCTION); 
     return (func_params); 
 }
 
-std::list<std::string>* symtable_element::get_class_fields () {
+std::list<symtable_element>* symtable_element::get_class_fields () {
     assert(c_id == T_CLASS);
     return (class_fields); 
 }
@@ -74,11 +78,51 @@ void symtable::put (std::string key, symtable_element value) {
 
 /*  scopes_list methods.                                                     */
 
+scopes_list::scopes_list(void) {
+    this->prev = NULL;
+    this->table = new symtable();
+}
+
+void scopes_list::push_symtable(void) {
+    this->prev = this;
+    this->table = new symtable();
+}
+void scopes_list::push_symtable(symtable_element s) {
+    assert ((s.get_class() == symtable_element::T_CLASS)
+         || (s.get_class() == symtable_element::T_FUNCTION));
+    
+    /*  First, a new (block) symbols table must be pushed.                   */
+    this->push_symtable();
+
+    /*  Second, each element in the function parameters list or class 
+        attributes and methods list must be added to the new symbols table.  */
+    std::list<symtable_element>* l;
+    if (s.get_class() == symtable_element::T_FUNCTION) 
+        l = s.get_func_params();
+    if (s.get_class() == symtable_element::T_CLASS) 
+        l = s.get_class_fields();
+
+    for(std::list<symtable_element>::iterator it = l->begin(); it != l->end(); it++) 
+        (this->table)->put(it->get_key(), *it);
+}
+
+void scopes_list::pop_symtable(void) {
+    if (this->prev == NULL) {
+        delete (this->table);
+        this->table = new symtable();
+    } else {
+        delete (this->table);
+        (this->table) = (this->prev)->table;
+        (this->prev) = (this->prev)->prev;
+    }
+}
+
+
 symtable_element scopes_list::get(std::string key) {
      /*  Must search from the top of the stack and downwards.        */
     for(scopes_list *it = this; it != NULL; it = it->prev) 
-        if((it->table).elem_exists(key))
-            return ((it->table).get(key));
+        if((it->table)->elem_exists(key))
+            return ((it->table)->get(key));
 
     /*  If the key has not been found in any of the symbols 
         tables, then it has not been found in the current scope.     */
@@ -88,7 +132,7 @@ symtable_element scopes_list::get(std::string key) {
 void scopes_list::put(std::string key, symtable_element value) {
     /*  Always insert a new identifier's information in the top
         of the stack.                                               */
-    table.put(key, value);
+    (this->table)-> put(key, value);
 }
 
 scopes_list::~scopes_list() {
--- src/symtable.h
+++ src/symtable.h
@@ -28,60 +28,71 @@ public:
                  , ID };
 
     /*  For the following constructors, only necessary information is given. 
-        Other attributes will be inferred from this information.             */
+        Other attributes will be inferred from this information.
+        The first parameter in each one of them, except for the first one,
+        is the key in the symbol table associated with this element.         */
 
     /*  To be returned when an entry is not found in a symbols table.
         Precondition: the argument is NOT_FOUND                              */
     symtable_element(id_class);
 
     /*  Basic type variable.                                                 */
-    symtable_element(id_type);
+    symtable_element(std::string, id_type);
 
     /*  Basic type array.                                                    */
-    symtable_element(id_type, unsigned int);
+    symtable_element(std::string, id_type, unsigned int);
 
     /*  Object.                                                              */
-    symtable_element(std::string*); 
+    symtable_element(std::string, std::string); 
 
     /*  Objects array.                                                       */
-    symtable_element(std::string*, unsigned int);
+    symtable_element(std::string, std::string, unsigned int);
 
     /*  Function.                                                            */
-    symtable_element(id_type, std::list<std::string>*); 
+    symtable_element(std::string, id_type, std::list<symtable_element>*); 
 
     /*  Class.                                                               */
-    symtable_element(std::string*, std::list<std::string>*);
+    symtable_element(std::string, std::list<symtable_element>*);
 
 
     /*  Getters.                                                             */
 
+    std::string get_key(void);
+
     /*  Returns the corresponding enum id_class value.                       */
-    id_class get_class ();
+    id_class get_class (void);
 
     /*  Returns the corresponding enum id_type value.                        */
-    id_type get_type ();
+    id_type get_type (void);
 
     /*  Precondition: get_class() == (T_OBJ || T_OBJ_ARRAY || T_CLASS).      */
-    std::string* get_class_type ();
+    std::string get_class_type (void);
 
     /*  Precondition: get_class() = (T_ARRAY || T_OBJ_ARRAY).                */
-    unsigned int get_dimension ();
+    unsigned int get_dimension (void);
 
     /*  Returns list of function formal parameters identifiers.
         Precondition: get_class() == T_FUNCTION.                             */
-    std::list<std::string>* get_func_params ();
+    std::list<symtable_element>* get_func_params (void);
 
     /*  Returns list of class fields and methods identifiers.
         Precondition: get_class() == T_CLASS.                                */
-    std::list<std::string>* get_class_fields ();
+    std::list<symtable_element>* get_class_fields (void);
 
 private:
+    /*  key is meant to store the same key that appears in a symbols table
+        associated with this symtable_element. key will be needed, e.g., when
+        inserting a function or class in a symbols table: every function 
+        parameter or class field or method should be inserted in the symbols
+        table next to their corresponding identifiers.                       */
+    std::string key;
+
     id_class c_id;
 
     id_type t_id;
 
-    /*  Identifier type when it is an object.                                */
-    std::string* class_type;
+    /*  Identifier type when it is an object, an object array or a class.    */
+    std::string class_type;
 
     /*  Identifier dimension. Only for arrays.                               */
     unsigned int dim = 0;
@@ -89,11 +100,11 @@ private:
     /*  List of formal arguments identifiers (meant for when the symbols 
         table element is a function). Note that every element in the list 
         should be of a basic type.                                           */
-    std::list<std::string>* func_params = NULL;
+    std::list<symtable_element>* func_params = NULL;
 
     /*  List of attributes and methods identifiers (meant for when the 
         symbols table element is a class).                                   */
-    std::list<std::string>* class_fields = NULL;
+    std::list<symtable_element>* class_fields = NULL;
 };
 
 
@@ -118,22 +129,27 @@ private:
     scope.                                                                   */
 class scopes_list {
 private:
-    symtable table;
+    symtable* table;
     scopes_list *prev;
 
 public:
     /*  This constructor is meant only for the empty stack.                  */
     scopes_list(void);
 
-    /*  The scopes_list* parameter should represent the previous top of the
-        symbol tables stack.                                                 */
-    scopes_list(scopes_list*);
+    /*  Create a new symbols table related to a block.                       */
+    void push_symtable(void);
+
+    /*  Create a new symbols table related to a function or class.
+        PRECONDITION: get_class_type() == (T_CLASS || T_FUNCTION)            */
+    void push_symtable(symtable_element);
+
+    void pop_symtable(void);
     
     symtable_element get (std::string);
 
     void put(std::string, symtable_element);
 
-    ~scopes_list();
+    ~scopes_list(void);
 };
 
 #endif
