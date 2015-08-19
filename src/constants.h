54de0c8bacc6e32c479c847fdbb92faceb553441
Mallku2 <mallkuernesto@gmail.com>
2015-08-19 13:31:21 -0300

    Type related constants

---
+++ src/constants.h
@@ -0,0 +1,14 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+// Initial values of attributes.
+const int integer_initial_value = 0;
+const float float_initial_value = 0.0;
+const bool boolean_initial_value = true;
+
+// Type's width (in bytes).
+const unsigned int integer_width = 4;
+const unsigned int float_width = 4;
+const unsigned int boolean_width = 4;
+
+#endif
